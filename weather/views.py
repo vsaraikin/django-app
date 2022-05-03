ae620bc773b16d3af9774dea7d530e1725134295
Vladimir Saraikin <saraikinvladimir228boss@gmail.com>
2022-05-03 23:29:54 +0300

    v1.0

--- weather/views.py
+++ weather/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 import requests
 from .models import City
+from .forms import CityForm
 
 api_key = 'b83d6195011516c807a895d59fb8a8a0'
 
@@ -8,7 +9,13 @@ api_key = 'b83d6195011516c807a895d59fb8a8a0'
 def index(request):
     url = 'http://api.openweathermap.org/data/2.5/weather?q={}&units=imperial&appid=' + api_key
 
-    cities = City.objects.all() #return all the cities in the database
+    cities = City.objects.all()  # return all the cities in the database
+
+    if request.method == 'POST':  # only true if form is submitted
+        form = CityForm(request.POST)  # add actual request data to form for processing
+        form.save()  # will validate and save if validate
+
+    form = CityForm()
 
     weather_data = []
 
@@ -25,6 +32,6 @@ def index(request):
 
         weather_data.append(weather)  # add the data for the current city into our list
 
-    context = {'weather_data': weather_data}
+    context = {'weather_data': weather_data, 'form': form}
 
     return render(request, 'weather/index.html', context)
