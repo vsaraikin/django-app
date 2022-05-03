879c54401e5f53acdef69b0f6dc427bb521e38fa
Vladimir Saraikin <saraikinvladimir228boss@gmail.com>
2022-05-03 20:11:53 +0300

    first commit

---
+++ weather/views.py
@@ -0,0 +1,30 @@
+from django.shortcuts import render
+import requests
+from .models import City
+
+api_key = 'b83d6195011516c807a895d59fb8a8a0'
+
+
+def index(request):
+    url = 'http://api.openweathermap.org/data/2.5/weather?q={}&units=imperial&appid=' + api_key
+
+    cities = City.objects.all() #return all the cities in the database
+
+    weather_data = []
+
+    for city in cities:
+        city_weather = requests.get(
+            url.format(city)).json()  # request the API data and convert the JSON to Python data types
+
+        weather = {
+            'city': city,
+            'temperature': city_weather['main']['temp'],
+            'description': city_weather['weather'][0]['description'],
+            'icon': city_weather['weather'][0]['icon']
+        }
+
+        weather_data.append(weather)  # add the data for the current city into our list
+
+    context = {'weather_data': weather_data}
+
+    return render(request, 'weather/index.html', context)
